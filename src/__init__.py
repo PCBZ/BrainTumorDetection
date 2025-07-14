5dd9a9a4089290b5a080ddfe5f4bdfae648bd9df
PCBZ <devil02047@outlook.com>
2025-07-14 13:36:49 -0700

    add preprocessing pipeline

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Brain Tumor Detection Project
-
-This package contains modules for brain tumor detection using deep learning.
-"""
-
-__version__ = "1.0.0"
-__author__ = "Your Name"
-__email__ = "your.email@example.com"
